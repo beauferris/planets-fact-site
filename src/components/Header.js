7cd82784346f78c8e8e22809cf38fd83c0688048
beauferris <h-beauferris@hotmail.com>
2021-06-21 12:04:44 -0600

    tablet version

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,15 +5,18 @@ function Header(props) {
 
     return (
         <header>
-            <button className = "home">THE PLANETS</button>
-            <button onClick={props.onClick} value="Mercury">MERCURY</button>
-            <button onClick={props.onClick} value="Venus">VENUS</button>
-            <button onClick={props.onClick} value="Earth">EARTH</button>
-            <button onClick={props.onClick} value="Mars">MARS</button>
-            <button onClick={props.onClick} value="Jupiter">JUPITER</button>
-            <button onClick={props.onClick} value="Saturn">SATURN</button>
-            <button onClick={props.onClick} value="Uranus">URANUS</button>
-            <button onClick={props.onClick} value="Neptune">NEPTUNE</button>
+            <button className="home">THE PLANETS</button>
+            <div className="buttonh">
+                <button onClick={props.onClick} value="Mercury">MERCURY</button>
+                <button onClick={props.onClick} value="Venus">VENUS</button>
+                <button onClick={props.onClick} value="Earth">EARTH</button>
+                <button onClick={props.onClick} value="Mars">MARS</button>
+                <button onClick={props.onClick} value="Jupiter">JUPITER</button>
+                <button onClick={props.onClick} value="Saturn">SATURN</button>
+                <button onClick={props.onClick} value="Uranus">URANUS</button>
+                <button onClick={props.onClick} value="Neptune">NEPTUNE</button>
+            </div>
+
         </header>
     )
 }
--- src/components/Information.js
+++ src/components/Information.js
@@ -2,22 +2,25 @@ import React from 'react';
 import './Info.css';
 
 function Information(props) {
-  
+
     return (
         <div className="middle">
 
-        <div className="planet-img">
-            <img src={(props.img)}></img>
-            <img class="zoom" style={{display:props.display}} src ={(props.img_zoom)}></img>
-        </div>
+            <div className="planet-img">
+                <img src={(props.img)} alt="oopps"></img>
+                <img className="zoom" style={{ display: props.display }} src={(props.img_zoom)} alt="oops"></img>
+            </div>
 
-        <div className="info">
-            <span className="title">{props.name}</span>
-            <span className="summary">{props.summary}</span>
-            <button onClick={props.onClick} value="overview" >01 Overview</button>
-            <button onClick={props.onClick} value="structure">02 Internal Structure</button >
-            <button onClick={props.onClick} value="geology">03 Surface Geology</button >
-        </div>
+            <div className="info">
+                <span className="title">{props.name}</span>
+                <span className="summary">{props.summary}</span>
+                <div className="buttons">
+                    <button onClick={props.onClick} value="overview" >01 Overview</button>
+                    <button onClick={props.onClick} value="structure">02 Internal Structure</button >
+                    <button onClick={props.onClick} value="geology">03 Surface Geology</button >
+                </div>
+
+            </div>
 
         </div>
     )
